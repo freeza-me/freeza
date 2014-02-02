c915adc70c3faef2a1394c0211a8ffcb78590bf9
Eido NABESHIMA <closer009@gmail.com>
2014-02-02 20:58:47 +0900

    asets_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,8 @@ Freeza::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
+  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
