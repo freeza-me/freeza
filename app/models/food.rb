9801b2b300b3fd0c3f4c4979d910036b2d98ce4b
Eido NABESHIMA <closer009@gmail.com>
2014-02-03 03:50:44 +0900

    colors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,13 @@
 class Food < ActiveRecord::Base
   belongs_to :fridge
 
+  default_scope -> { order(:deadline) }
+
   validates :fridge_id, presence: true
   validates :name,      presence: true
   validates :deadline,  presence: true
+
+  def limit
+    (self.deadline - Date.today).to_i
+  end
 end
