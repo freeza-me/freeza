e61aa4c475fd78bb7d0a60494588f1f038e86002
Eido NABESHIMA <closer009@gmail.com>
2014-02-02 19:27:25 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freeza::Application.config.secret_key_base = 'd303544c831004f1ab4d578a9593e7921ed131a029e0d5abc52e7e2b424ac086807feb83e313850b016fd9e5dd59110371c1b581c369a2a1995ebadcb8634d0e'
