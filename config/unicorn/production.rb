1fb96054c5d3f847e897cfcae64b4bf9232ce09f
Eido NABESHIMA <closer009@gmail.com>
2014-02-02 22:35:14 +0900

    unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,31 +1,20 @@
-# config/unicorn.rb
-
-app_path = "/var/www/comfort-davines/current"
-
-# working_directory app_path
-
-stderr_path "#{app_path}/log/unicorn.log"
-stdout_path "#{app_path}/log/unicorn.log"
-
-pid "#{app_path}/tmp/pids/davines_app.pid"
-
-listen "/tmp/davines_app.sock"
-worker_processes 3
-timeout 180
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
 preload_app true
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill :QUIT, File.read(old_pid).to_i
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
   end
+
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
