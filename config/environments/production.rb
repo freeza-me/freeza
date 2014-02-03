f5ae5a10b53693c04675b00ef2627dc26266233b
Eido NABESHIMA <closer009@gmail.com>
2014-02-03 22:49:34 +0900

    disable rack::cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,11 +18,11 @@ Freeza::Application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
-  config.action_dispatch.rack_cache = {
-    verbose: true,
-    metastore:  "#{ENV['REDIS_URL']}/1/metastore",
-    ntitystore: "#{ENV['REDIS_URL']}/1/entitystore"
-  }
+  # config.action_dispatch.rack_cache = {
+  #   verbose: true,
+  #   metastore:  "#{ENV['REDIS_URL']}/1/metastore",
+  #   ntitystore: "#{ENV['REDIS_URL']}/1/entitystore"
+  # }
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
