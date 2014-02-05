4febb86e39bccf0d4cf6044d266e6169aefb067c
Eido NABESHIMA <closer009@gmail.com>
2014-02-06 06:19:35 +0900

    Array

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -66,6 +66,7 @@ class FridgesController < ApplicationController
 
   def inbound
     if params[:mandrill_events] && data = ActiveSupport::JSON.decode(params[:mandrill_events])
+      data = data.first if data.is_a? Array
       if data['event'] && data['event'] == 'inbound'
         if (m = data['msg']['to'].match(/^fridge-(.*)@freeza.me$/))[0] && token = m[1]
           if @fridge = Fridge.where(inbound_token: token).first
