ca50569d4c51c638909b7113c054f112d8a36b18
Eido NABESHIMA <closer009@gmail.com>
2014-02-05 18:05:31 +0900

    i18n readable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  LOCALE_READABLE = {
+    ja: '日本語',
+    en: 'English'
+  }
+
+  def available_locales_with_readable
+    I18n.available_locales.map do |locale|
+      [ LOCALE_READABLE[locale.to_sym], locale ]
+    end
+  end
+
 end
